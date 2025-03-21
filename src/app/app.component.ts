8b75a9b91877a5cebe7045ed5d47f93f8964943d
Rahul Kumar <rahulkumarair@Rahuls-Laptop.local>
2025-03-21 22:13:21 +0530

    [Rahul Kumar] Added:Validation on User Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,20 @@ export class AppComponent {
   imgUrl="../assets/BL_Logo.jpg";
   url="https://www.bridgelabz.com";
   userName :string="";
+  nameError:string="";
 
 onClick($event: Event) {
   console.log("open breidgelabz site from logo",$event);
   window.open(this.url,"_blank");
 }
+onInput($event:Event){
+  console.log("Change Event Occured!",$event);
+  const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+  if(nameRegex.test(this.userName)){
+    this.nameError ="";
+    return ;
+  }
+  this.nameError ="Name is Incorrect";
+}
+
 }
